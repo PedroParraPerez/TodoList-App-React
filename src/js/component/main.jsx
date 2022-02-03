07c2db5689ed0a56ca423f215dbc12d2d358d7a5
PedroParraPerez <pedroparra06091994@gmail.com>
2022-02-03 11:10:50 +0000

    start

---
+++ src/js/component/main.jsx
@@ -0,0 +1,17 @@
+
+import React, { useEffect, useState }  from "react";
+
+//include images into your bundle
+
+
+//create your first component
+const Main = () => {
+
+	
+	return (
+		<>
+		</>
+	);
+};
+
+export default Main;
